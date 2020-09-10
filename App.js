1c6c58f42d98d3e240038abc353fbe5606636106
Aravind Chowdary <kamaravichow@gmail.com>
2020-09-10 22:15:21 +0530

    made initial screens

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack';
+import LoadingScreen from './screens/LoadingScreen'
+import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import * as firebase from 'firebase'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+var firebaseConfig = {
+  apiKey: "AIzaSyDckCSN0VE1gs3qJyYsjf3n4GRdRcWQFno",
+  authDomain: "momo-india.firebaseapp.com",
+  databaseURL: "https://momo-india.firebaseio.com",
+  projectId: "momo-india",
+  storageBucket: "momo-india.appspot.com",
+  messagingSenderId: "164779946628",
+  appId: "1:164779946628:web:61faff1d769d6c7ed588d1",
+  measurementId: "G-17GDQ60V6G"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const AppStack = createStackNavigator({
+  Home : HomeScreen
+})
+
+const AuthStack = createStackNavigator({
+  Login : LoginScreen,
+  Register : RegisterScreen
+})
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      Loading : LoadingScreen,
+      App : AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "Loading"
+    }
+  )
+)
